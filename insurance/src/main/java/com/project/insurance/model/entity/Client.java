2d252a4e63f3008f4dd559111d586a7e176044a0
sS1mon <seleckysimon@gmail.com>
2023-04-18 09:48:07 +0200

    checking if retyped password matches logic created

--- insurance/src/main/java/com/project/insurance/model/entity/Client.java
+++ insurance/src/main/java/com/project/insurance/model/entity/Client.java
@@ -23,6 +23,7 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull(message = "Role isn't assin")
     @Column(name = "role", nullable = false,length = 10)
     private Roles role;
     @NotNull(message = "Please fill the name!")
@@ -37,6 +38,7 @@ public class Client {
     private String email;
     @Column(name = "phone_number", unique = true)
     private Long phoneNumber;
+    @Column(name = "password", nullable = false, length = 200)
     private String password;
 
     @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
@@ -51,11 +53,20 @@ public class Client {
         this.phoneNumber = phoneNumber;
     }
 
-    public Client (ClientRegDto reg){
+    public Client (ClientRegDto reg, Roles role){
         this.name = reg.getName();
         this.sureName = reg.getSurName();
         this.dayOfBirth = reg.getDayOfBirth();
         this.email = reg.getEmail();
         this.phoneNumber = reg.getPhoneNumber();
+        this.password = reg.getPassword();
+        if (role.name().equals(Roles.CLIENT.name())){
+            this.role = Roles.CLIENT;
+        } else if (role.name().equals(Roles.EMPLOYEE.name())) {
+            this.role = Roles.EMPLOYEE;
+        } else {
+            this.role = Roles.ADMIN;
+        }
+
     }
 }
--- insurance/src/main/java/com/project/insurance/service/ClientServiceImpl.java
+++ insurance/src/main/java/com/project/insurance/service/ClientServiceImpl.java
@@ -1,14 +1,15 @@
 package com.project.insurance.service;
 
-import com.project.insurance.enums.Type;
 import com.project.insurance.model.dto.ClientRegDto;
 import com.project.insurance.model.dto.ClientResponseDto;
 import com.project.insurance.model.dto.InsureCarDto;
 import com.project.insurance.model.entity.CarInsurance;
 import com.project.insurance.model.entity.Client;
+import com.project.insurance.model.entity.Roles;
 import com.project.insurance.repository.CarInsuranceRepository;
 import com.project.insurance.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,20 +17,32 @@ public class ClientServiceImpl implements ClientService {
 
     private final ClientRepository clientRepository;
     private final CarInsuranceRepository carInsuranceRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public ClientServiceImpl(ClientRepository clientRepository, CarInsuranceRepository carInsuranceRepository) {
+    public ClientServiceImpl(ClientRepository clientRepository, CarInsuranceRepository carInsuranceRepository, PasswordEncoder passwordEncoder) {
         this.clientRepository = clientRepository;
         this.carInsuranceRepository = carInsuranceRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public ClientResponseDto clientRegistration(ClientRegDto clientRegDto) {
         Client client = clientRepository.findByEmail(clientRegDto.getEmail());
         if (client == null) {
-            client = new Client(clientRegDto);
-            clientRepository.save(client);
-            String txt = "Dear " + client.getName() + " " + client.getSureName() + " thanks for your trust, we will keep you updated";
-                    return new ClientResponseDto(txt);
+            if (clientRegDto.getPassword().matches(clientRegDto.getRepeatPassword())) {
+                if (carInsuranceRepository.findAll().size() == 0) {
+                    client = new Client(clientRegDto, Roles.ADMIN);
+
+                } else {
+                    client = new Client(clientRegDto, Roles.CLIENT);
+                }
+                clientRepository.save(client);
+                String txt = "Dear " + client.getName() + " " + client.getSureName() + " thanks for your trust, we will keep you updated.";
+                return new ClientResponseDto(txt);
+            }
+            else {
+                throw new IllegalStateException("Passwords aren't matching!");
+            }
         } else {
             throw new IllegalStateException("Email is already registered!");
         }
